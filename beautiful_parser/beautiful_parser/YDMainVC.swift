669cfbab32b3408cf0b71e12edacfa4468dd69fe
Rusty Robot <rusty.robot@local>
2018-11-06 17:00:54 +0000

    added icon and first VC

---
+++ beautiful_parser/beautiful_parser/YDMainVC.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+class YDMainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "🐝 parser 🐝"
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
